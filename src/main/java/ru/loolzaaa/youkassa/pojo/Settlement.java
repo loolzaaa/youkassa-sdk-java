5433cb762b63dc6a918b1689fc7a9dd233e1685a
Andrey Korsakov <loolzaaa@gmail.com>
2025-09-22 21:09:08 +0500

    Update Settlement validation
    - add some types

--- src/main/java/ru/loolzaaa/youkassa/pojo/Settlement.java
+++ src/main/java/ru/loolzaaa/youkassa/pojo/Settlement.java
@@ -23,9 +23,19 @@ public class Settlement implements Validated {
 
     @Override
     public void validate() {
+        //TODO: check type values by available in subclass
         if (type == null || amount == null) {
             throw new IllegalArgumentException("Type and amount must not be null");
         }
         amount.validate();
+    }
+
+    public static class Type {
+        public static final String PAYOUT = "payout";
+
+        public static final String CASHLESS = "cashless";
+        public static final String PREPAYMENT = "prepayment";
+        public static final String POSTPAYMENT = "postpayment";
+        public static final String CONSIDERATION = "consideration";
     }
 }
