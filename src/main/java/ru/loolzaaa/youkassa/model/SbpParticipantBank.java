e46c862e97c6a9ee36d1706b95ee5239c8f93dfc
Andrey Korsakov <loolzaaa@gmail.com>
2025-09-25 20:36:51 +0500

    Create SbpParticipantBank validation

---
+++ src/main/java/ru/loolzaaa/youkassa/model/SbpParticipantBank.java
@@ -0,0 +1,25 @@
+package ru.loolzaaa.youkassa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ru.loolzaaa.youkassa.client.RequestBody;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SbpParticipantBank implements RequestBody {
+    @JsonProperty("bank_id")
+    private String bankId;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("bic")
+    private String bic;
+}
---
+++ src/main/java/ru/loolzaaa/youkassa/processors/SbpParticipantBankProcessor.java
@@ -0,0 +1,32 @@
+package ru.loolzaaa.youkassa.processors;
+
+import lombok.RequiredArgsConstructor;
+import ru.loolzaaa.youkassa.client.ApiClient;
+import ru.loolzaaa.youkassa.model.SbpParticipantBank;
+
+/**
+ * Processor for {@link SbpParticipantBank} entity.
+ * <p>
+ * The API allows you to receive information
+ * about SBP participants.
+ * <p>
+ * Use {@link ApiClient} for API server communication.
+ */
+
+@RequiredArgsConstructor
+public class SbpParticipantBankProcessor {
+
+    private static final String BASE_PATH = "/sbp_banks";
+
+    private final ApiClient client;
+
+    /**
+     * Receive information about SBP participants.
+     *
+     * @return entity that represents SBP participants
+     */
+
+    public SbpParticipantBank findMe() {
+        return client.sendRequest("GET", BASE_PATH, null, null, SbpParticipantBank.class);
+    }
+}
